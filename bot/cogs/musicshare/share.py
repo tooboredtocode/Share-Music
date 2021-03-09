f28f63455045b617fc4dda39c5f3bff524d48059
tooboredtocode <bored-coder@tooboredtocode.dev>
2021-03-09 17:18:04 +0100

    feat: remove reliability on the original url's data

--- bot/cogs/musicshare/share.py
+++ bot/cogs/musicshare/share.py
@@ -38,48 +38,79 @@ class Share(commands.Cog):
 
     @commands.command()
     async def share(self, ctx, url: str):
+
+        # try to delete the original message
         try:
             await ctx.message.delete()
         except discord.errors.Forbidden:
             pass
 
+        # filter out bad requests
         if not pattern.match(url):
             await ctx.send("Please send a valid url", delete_after=15)
             return
 
+        # send placeholder message
         message = await ctx.send("Please wait a moment...")
 
-        result = requests.get(f"https://api.song.link/v1-alpha.1/links?url={url}")
+        # get the info from song.link
+        response = requests.get(f"https://api.song.link/v1-alpha.1/links?url={url}")
 
-        if result.status_code != 200:
-            await ctx.send("Error getting links", delete_after=15)
+        # inform user about error
+        if response.status_code != 200:
+            await message.edit(content="Error getting links", delete_after=15)
             return
 
-        result = result.json()
+        # turn the request into a dict
+        result = response.json()
 
+        # get the links
         links = list()
-
         for source in result["linksByPlatform"]:
             try:
                 links.append(f"[{sources[source]}]({result['linksByPlatform'][source]['url']})")
             except KeyError:
                 links.append(f"[{source}]({result['linksByPlatform'][source]['url']})")
 
-        thumbnails = dict()
-
+        # get the song/album info
+        song_album_info = dict()
         for provider in result["entitiesByUniqueId"]:
-            thumbnails[f"{result['entitiesByUniqueId'][provider]['apiProvider']}"]\
-                = f"{result['entitiesByUniqueId'][provider]['thumbnailUrl']}"
+            song_album_info[f"{result['entitiesByUniqueId'][provider]['apiProvider']}"] \
+                = {
+                "artist": result["entitiesByUniqueId"][provider]["artistName"],
+                "title": result["entitiesByUniqueId"][provider]["title"],
+                "thumbnail": result["entitiesByUniqueId"][provider]["thumbnailUrl"]
+            }
 
-        if "itunes" in thumbnails:
-            thumbnail = thumbnails["itunes"]
-        elif "spotify" in thumbnails:
-            thumbnail = thumbnails["spotify"]
+        # set the artist, title and thumbnail variables, since the data provided by youtube isn't very good
+        if "itunes" in song_album_info:
+            artist = song_album_info["itunes"]["artist"]
+            title = song_album_info["itunes"]["title"]
+            thumbnail = song_album_info["itunes"]["thumbnail"]
+        elif "spotify" in song_album_info:
+            artist = song_album_info["itunes"]["artist"]
+            title = song_album_info["itunes"]["title"]
+            thumbnail = song_album_info["itunes"]["thumbnail"]
+        elif "tidal" in song_album_info:
+            artist = song_album_info["tidal"]["artist"]
+            title = song_album_info["tidal"]["title"]
+            thumbnail = song_album_info["tidal"]["thumbnail"]
+        elif "yandex" in song_album_info:
+            artist = song_album_info["yandex"]["artist"]
+            title = song_album_info["yandex"]["title"]
+            thumbnail = song_album_info["yandex"]["thumbnail"]
+        elif "soundcloud" in song_album_info:
+            artist = song_album_info["soundcloud"]["artist"]
+            title = song_album_info["soundcloud"]["title"]
+            thumbnail = song_album_info["soundcloud"]["thumbnail"]
         else:
+            artist = result["entitiesByUniqueId"][result["entityUniqueId"]]["artistName"]
+            title = result["entitiesByUniqueId"][result["entityUniqueId"]]["title"]
             thumbnail = result["entitiesByUniqueId"][result["entityUniqueId"]]["thumbnailUrl"]
 
+        # create the discord embed
         embed = discord.Embed.from_dict({
-            "title": result["entitiesByUniqueId"][result["entityUniqueId"]]["title"],
+            "title": title,
             "type": "rich",
             "description": f"{' | '.join(links)}",
             "url": f"{result['pageUrl']}",
@@ -90,14 +121,15 @@ class Share(commands.Cog):
                 "url": thumbnail
             },
             "author": {
-                "name": result["entitiesByUniqueId"][result["entityUniqueId"]]["artistName"]
+                "name": artist
             },
             "fields": [{
                 "name": "sent by",
                 "value": ctx.author.mention
             }]
         })
-         
+
+        # try to edit the original message and if that fails send a new one
         try:
             await message.edit(content="", embed=embed)
         except discord.errors.NotFound:
