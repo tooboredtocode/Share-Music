e3f2ea99881a73690e3a0ca4506e8be03143d10b
tooboredtocode <bored-coder@tooboredtocode.dev>
2021-03-09 01:12:50 +0100

    fix: async functions not awaited

--- bot/cogs/musicshare/credits.py
+++ bot/cogs/musicshare/credits.py
@@ -8,17 +8,17 @@ class Credits(commands.Cog):
 
     @commands.command()
     async def invite(self, ctx):
-        ctx.send("Invite the bot here:\n"
+        await ctx.send("Invite the bot here:\n"
                  "https://discord.com/api/oauth2/authorize?client_id=818620296410955808&permissions=0&scope=bot")
 
     @commands.command()
     async def source(self, ctx):
-        ctx.send("You can find the code running this bot here:\n"
+        await ctx.send("You can find the code running this bot here:\n"
                  "https://github.com/tooboredtocode/Share-Music")
 
     @commands.command()
     async def credits(self, ctx):
-        ctx.send("Built by albedo#9999\n"
+        await ctx.send("Built by albedo#9999\n"
                  "Powered by https://odesli.co")
 
 
