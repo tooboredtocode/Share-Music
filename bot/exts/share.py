4efac546d0c955bdd33cb859f3dec42b6a6641de
tooboredtocode <bored-coder@tooboredtocode.dev>
2021-06-17 13:48:29 +0200

    feat: add error handling in case of improper response

--- bot/exts/share.py
+++ bot/exts/share.py
@@ -7,21 +7,26 @@ from discord_slash import cog_ext, SlashContext
 from discord_slash.model import SlashCommandOptionType
 from discord_slash.utils.manage_commands import create_option
 from io import BytesIO
+from json import JSONDecodeError
+from loguru import logger
 from random import randint
+from typing import Literal
 from PIL import Image
 
 from bot.factory import Bot
 
-pattern = re.compile("^https:\/\/(?:"
-                     ".*amazon\.com|"
-                     ".*deezer\.com|"
-                     ".*music\.apple\.com|"
-                     ".*pandora.*\.com|"
-                     "soundcloud\.com|"
-                     ".*spotify\.com|"
-                     ".*tidal\.com|"
-                     ".*music\.yandex\..{1,3}|"
-                     ".*youtu(?:\.be|be\.com))")
+pattern = re.compile(
+    "^https:\/\/(?:"
+    ".*amazon\.com|"
+    ".*deezer\.com|"
+    ".*music\.apple\.com|"
+    ".*pandora.*\.com|"
+    "soundcloud\.com|"
+    ".*spotify\.com|"
+    ".*tidal\.com|"
+    ".*music\.yandex\..{1,3}|"
+    ".*youtu(?:\.be|be\.com))"
+)
 
 source_identifier_to_name = {
     "amazonMusic": "Amazon Music",
@@ -39,14 +44,13 @@ source_identifier_to_name = {
     "youtubeMusic": "YouTube Music"
 }
 
-source_priority = {
-    "itunes": 1,
-    "spotify": 2,
-    "tidal": 3,
-    "yandex": 4,
-    "soundcloud": 5,
-    "original_provider": 6
-}
+source_priority = [
+    "itunes",
+    "spotify",
+    "tidal",
+    "yandex",
+    "soundcloud"
+]
 
 
 class Share(commands.Cog):
@@ -60,13 +64,15 @@ class Share(commands.Cog):
                 self.session.connector.close()
             self.session._connector = None
 
-    @classmethod
-    async def get_dominant_colours(cls, session: aiohttp.ClientSession,  url: str):
+    async def get_dominant_colours(self, url: str) -> tuple[int, int, int]:
+
+        if not url:
+            return 0, 0, 0
 
         # get the image from the url
-        async with session.get(url) as response:
+        async with self.session.get(url) as response:
             if response.status != 200:
-                return 0
+                return 0, 0, 0
             thumbnail = Image.open(BytesIO(await response.read()))
 
             # downsize the image to increase processing and turn it into a palette
@@ -107,45 +113,65 @@ class Share(commands.Cog):
         async with self.session.get(f"https://api.song.link/v1-alpha.1/links?url={url}") as response:
             # inform user about error
             if response.status != 200:
-                await ctx.send(content="Error getting links", delete_after=15)
+                await ctx.send(content="Error getting links, song.link couldn't respond", delete_after=15)
+                logger.info(
+                    f"Couldn't get response from song.link for url: {url} "
+                    f"song.link responded with code: {response.status}"
+                    )
                 return
 
             # turn the request into a dict
-            result = await response.json()
+            try:
+                result = await response.json()
+            except JSONDecodeError as e:
+                await ctx.send(content="Error getting links, song.link returned an unexpected response", delete_after=15)
+                logger.opt(exception=True).warning(f"song.link returned a faulty response for url: {url}, error: {e}")
+                return
 
-            # get the links and store them with the markdown syntax already applied
-            links = []
-            for source, link in result["linksByPlatform"].items():
-                title = source_identifier_to_name[source] if source in source_identifier_to_name else source
-                url = link["url"]
+            # wrap the whole thing in a try loop if certain keys aren't found
+            try:
+                # get the links and store them with the markdown syntax already applied
+                links = []
+                for source, link in result["linksByPlatform"].items():
+                    title = source_identifier_to_name.get(source) or source
+                    url = link["url"]
 
-                links.append(f"[{title}]({url})")
+                    links.append(f"[{title}]({url})")
 
-            links.sort(key=lambda chars: chars.upper())
+                links.sort(key=lambda chars: chars.upper())
 
-            # get important parts from the api response
-            reduced_info = {}
-            for key, value in result["entitiesByUniqueId"].items():
-                provider = "original_provider" if key == result["entityUniqueId"] and value["apiProvider"] not in source_priority else value["apiProvider"]
-                if provider not in source_priority:
-                    continue
-                try:
-                    reduced_info[source_priority[provider]] = {
-                        "artist": value["artistName"],
-                        "title": value["title"],
-                        "thumbnail": value["thumbnailUrl"]
-                    }
-                except KeyError:
-                    pass
+                # Fix some nasty references later
+                artist = None
+                title = None
+                thumbnail = None
+                colour_int = None
 
-            # sort the dict
-            reduced_info = list(map(lambda key: reduced_info[key], sorted(reduced_info)))
+                # get important parts from the api response
+                sp = source_priority.copy()
+                sp.append(result["entitiesByUniqueId"][result["entityUniqueId"]]["apiProvider"])
+                current_max = len(sp) - 1
+                for key, value in result["entitiesByUniqueId"].items():
+                    if not ((provider := value["apiProvider"]) in sp):
+                        continue
 
-            # get the information
-            artist, title, thumbnail = [reduced_info[0][key] for key in ["artist", "title", "thumbnail"]]
+                    if (priority := sp.index(provider)) > current_max:
+                        continue
+                    current_max = priority
+
+                    try:
+                        artist = value["artistName"]
+                        title = value["title"]
+                        thumbnail = value["thumbnailUrl"]
+                    except KeyError:
+                        pass
+
+            except KeyError as e:
+                await ctx.send(content="Error getting links, song.link returned unexpected response", delete_after=15)
+                logger.opt(exception=True).warning(f"song.link returned a faulty response for url: {url}, KeyError: {e}")
+                return
 
             # get the dominant colours
-            colour = await self.get_dominant_colours(self.session, thumbnail)
+            colour = await self.get_dominant_colours(thumbnail)
             colour_int = (colour[0] << 16) + (colour[1] << 8) + colour[2]
 
             # create the discord embed
